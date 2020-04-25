c4095dfb113948d377330e58e93564120ee3f7a1
Niko Adler <nhiko3@gmail.com>
2020-04-25 10:08:52 +0200

    lot of shit

--- Assets/_Scripts/DataContainers/Order.cs
+++ Assets/_Scripts/DataContainers/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace DataContainers
 {
@@ -9,14 +10,100 @@ namespace DataContainers
         public string id;
         public string owner;
         public string restaurant;
-        public int table;
+        public string table;
         public Dictionary<string, Subborder> subborders;
+
+        public Dictionary<string, OrderItem> GetAllItems(Menu iMenu)
+        {
+            Dictionary<string, OrderItem> items = new Dictionary<string, OrderItem>();
+            foreach (var suborder in subborders)
+            {
+                foreach (var item in suborder.Value.items)
+                {
+                    if (items.ContainsKey(item.Key))
+                    {
+                        items[item.Key].amount += item.Value.amount;
+                        items[item.Key].price += item.Value.amount * iMenu.foods[item.Key].price;
+                    }
+                    else
+                    {
+                        items.Add(item.Key, new OrderItem());
+                        items[item.Key].amount = item.Value.amount;
+                        items[item.Key].id = item.Value.id;
+                        items[item.Key].price = item.Value.amount * iMenu.foods[item.Key].price;
+                        items[item.Key].name = iMenu.foods[item.Key].name;
+                    }
+                }
+            }
+            return items;
+        }
+
+        public Order(JSONObject iJson)
+        {
+            Debug.Log(iJson["activeUsers"].ToString());
+            activeUsers = new List<string>();
+            subborders = new Dictionary<string, Subborder>();
+            foreach (var item in iJson["activeUsers"].keys)
+            {
+                activeUsers.Add(item);
+                subborders.Add(item, new Subborder(iJson["suborders"][item]));
+            }
+            id = iJson["id"].str;
+            owner = iJson["owner"].str;
+            restaurant = iJson["restaurant"].str;
+            table = iJson["table"].str;
+
+        }
+
+        public override string ToString()
+        {
+            string s = "Owner: " + owner + " Restaurant: " + restaurant + " Table: " + table + " Id: " + id;
+            foreach (var item in subborders)
+            {
+                s += "\nSuborder: " + item.Key + "\n" + item.Value.ToString();
+            }
+            return s; 
+        }
     }
 
     public class Subborder
     {
-        public List<OrderItem> items;
+        public Dictionary<string, OrderItem> items;
         public string status;
+
+        public Subborder(JSONObject iJson)
+        {
+            Debug.Log(iJson.ToString());
+            status = iJson["status"].str;
+            items = new Dictionary<string, OrderItem>();
+            if (iJson["items"] != null)
+            {
+                foreach (var item in iJson["items"].list)
+                {
+                    Debug.Log(item.ToString());
+
+                    if (item == null || item.ToString().Equals("null"))
+                    {
+                        Debug.Log("jeee");
+
+                        continue;
+                    }
+                    items.Add(item["id"].ToString(), new OrderItem(item));
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = "Status: " + status;
+            foreach (var item in items)
+            {
+                s += "\nItem: " + item.Key + "\n" + item.Value.ToString();
+            }
+            return s;
+        }
     }
+
+   
 }
 
--- Assets/_Scripts/DataContainers/OrderItem.cs
+++ Assets/_Scripts/DataContainers/OrderItem.cs
@@ -7,6 +7,35 @@ namespace DataContainers {
         public string name;
         public int amount;
         public float price;
+        public string id;
+        private int ordered;
+        private int delivered;
+        private int transfered;
+
+        public OrderItem(JSONObject iJson)
+        {
+            name = iJson["name"].str;
+            var v = iJson["price"];
+            price = float.Parse(iJson["price"].str.Replace(',', '.'));
+            id = iJson["id"].ToString();
+            ordered = (iJson["ordered"] == null) ? 0 : int.Parse(iJson["ordered"].ToString());
+            delivered = (iJson["delivered"] == null) ? 0 : int.Parse(iJson["delivered"].ToString());
+            transfered = (iJson["transfered"] == null) ? 0 : int.Parse(iJson["transfered"].ToString());
+            amount = ordered + delivered + transfered;
+        }
+
+        public OrderItem()
+        {
+            ordered = 0;
+            delivered = 0;
+            transfered = 0;
+        }
+
+        public override string ToString()
+        {
+            string s = "Name: " + name + " amount: " + amount + " price: " + price + " Id: " + id + " ordered: " + ordered + " delivered: " + delivered + " transfered: " + transfered;
+            return s;
+        }
     }
 }
 
